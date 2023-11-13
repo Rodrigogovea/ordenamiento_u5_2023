5908e8f223a9fe6a2cc6555c29e944cf846bac88
rodrigogovea <rodrigogovea@live.com.mx>
2023-11-13 12:18:14 -0600

    add a new line

--- src/ordenamientos_u5/Ordenamientos_u5.java
+++ src/ordenamientos_u5/Ordenamientos_u5.java
@@ -9,8 +9,8 @@ public class Ordenamientos_u5
         int[] miArreglo = {5,2,10,3,4,45,23,67,12,56,3,200,400,1};
         
         System.out.println("Arreglo sin ordenar: ");
-        
         metodos.imprimir(miArreglo);
+        System.out.println("");
         miArreglo = metodos.burbuja(miArreglo);
         
         System.out.println("Arreglo ordenado: ");
