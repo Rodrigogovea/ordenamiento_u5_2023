3ddc230a4e174a57ed82a9fa3a3dbc1f9ba8dc96
rodrigogovea <rodrigogovea@live.com.mx>
2023-11-14 11:25:21 -0600

    se elimino código

--- src/ordenamientos_u5/Ordenamientos_u5.java
+++ src/ordenamientos_u5/Ordenamientos_u5.java
@@ -4,7 +4,6 @@ public class Ordenamientos_u5
 {
     public static void main(String[] args) 
     {
-        // TODO code application logic here
         metodos_ordenamientos metodos = new metodos_ordenamientos();
         int[] miArreglo = {5,2,10,3,4,45,23,67,12,56,3,200,400,1};
         
